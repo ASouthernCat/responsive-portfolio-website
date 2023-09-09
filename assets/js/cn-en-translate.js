a0674e5491859345bdc835337c6d42731de59706
ASouthernCat <103131106+ASouthernCat@users.noreply.github.com>
2023-09-09 17:24:39 +0800

    Update filepath cn-en-translate.js

--- assets/js/cn-en-translate.js
+++ assets/js/cn-en-translate.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var defaultLang = "en";
     $("[i18n]").i18n({
         defaultLang: defaultLang,
-        filePath: "/responsive-portfolio-website/assets/i18n/",//路径配置
+        filePath: "/assets/i18n/",//路径配置
         filePrefix: "i18n_",
         fileSuffix: "",
         forever: true,
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
         $("[i18n]").i18n({
             defaultLang: a,
-            filePath: "/responsive-portfolio-website/assets/i18n/",
+            filePath: "/assets/i18n/",
         });
 
     });
